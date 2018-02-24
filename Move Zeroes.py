c2c640db40993357c87bce56dab5d70d86adb809
hecter <hecter654321@gmail.com>
2018-02-24 11:50:01 +0800

    pass testcase: testSample by using remove zero element in list

--- Move Zeroes.py
+++ Move Zeroes.py
@@ -5,12 +5,25 @@ class unitest(unittest.TestCase):
         Input = []
         Output = []
         self.assertEqual(Solution().moveZeroes(Input),Output);
+    def testSample(self):
+        Input = [0, 1, 0, 3, 12]
+        Output = [1, 3, 12, 0, 0]
+        self.assertEqual(Solution().moveZeroes(Input),Output);
 
 class Solution():
     def moveZeroes(self, nums):
         if nums == []:
-            return []
-        return None
+            return nums
+        n = len(nums)
+        zero = 0
+        for node in nums:
+            if node == 0:
+                zero += 1
+                nums.remove(0)
+        for i in range(zero):
+            nums.append(0)
+        #print(nums)
+        return nums
 
 if __name__ == '__main__':
     unittest.main()
